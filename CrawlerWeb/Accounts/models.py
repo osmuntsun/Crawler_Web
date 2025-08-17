64bd7769daf736cc4821d0eca476a2032e674805
osmuntsun <osmuntsun900606@gmail.com>
2025-08-18 03:09:23 +0800

    修改中文亂碼

--- CrawlerWeb/Accounts/models.py
+++ CrawlerWeb/Accounts/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
 class User(AbstractUser):
-    """��芸��蝢拍�冽�嗆芋���"""
-    email = models.EmailField(unique=True, verbose_name='��餃����萎辣')
-    phone = models.CharField(max_length=15, blank=True, null=True, verbose_name='���璈����蝣�')
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='��剖��')
-    bio = models.TextField(max_length=500, blank=True, verbose_name='���鈭箇陛隞�')
-    date_joined = models.DateTimeField(default=timezone.now, verbose_name='閮餃��������')
-    last_login = models.DateTimeField(auto_now=True, verbose_name='���敺���餃��')
+    """自定義用戶模型"""
+    email = models.EmailField(unique=True, verbose_name='電子郵件')
+    phone = models.CharField(max_length=15, blank=True, null=True, verbose_name='手機號碼')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='頭像')
+    bio = models.TextField(max_length=500, blank=True, verbose_name='個人簡介')
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name='註冊時間')
+    last_login = models.DateTimeField(auto_now=True, verbose_name='最後登入')
     
     class Meta:
-        verbose_name = '��冽��'
-        verbose_name_plural = '��冽��'
+        verbose_name = '用戶'
+        verbose_name_plural = '用戶'
     
     def __str__(self):
         return self.username
